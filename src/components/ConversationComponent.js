6b4d02f20fefcc5fd96479f1a1c31bad1d85e941
abhiram11 <abhiramsatpute@gmail.com>
2021-07-31 17:19:25 +0530

    added text functioning, emoji picker, and done with ui

--- src/components/ConversationComponent.js
+++ src/components/ConversationComponent.js
@@ -1,8 +1,34 @@
 import "./ConversationComponent.css";
+import Picker from "emoji-picker-react";
 import { messagesList } from "../mockData";
+import { useState } from "react";
 
 const ConversationComponent = (props) => {
   const { chat } = props;
+  const [text, setText] = useState(""); // used 2 places: changes in input text
+  const [pickerVisible, togglePicker] = useState(false);
+  const [messageList, setMessageList] = useState(messagesList);
+
+  const onEmojiClick = (event, emojiObj) => {
+    //just add emoji to text
+    setText(text + emojiObj.emoji);
+  };
+
+  const onEnterPress = (event) => {
+    if (event.key === "Enter") {
+      const messages = [...messageList];
+      messages.push({
+        id: 0,
+        messageType: "TEXT",
+        text,
+        senderID: 0,
+        addedOn: "11:11 PM",
+      });
+      setMessageList(messages);
+      setText("");
+    }
+  };
+
   return (
     <div className="conversation">
       <div className="profileHeader">
@@ -20,7 +46,7 @@ const ConversationComponent = (props) => {
         {/* <div className={props.isYours ? "messageDiv1" : "messageDiv2"}>
           <div className={props.isYours ? "message1" : "message2"}>Hiiiii</div>
         </div> */}
-        {messagesList.map((messageData) => (
+        {messageList.map((messageData) => (
           <div
             className={
               messageData?.senderID === 0 ? "messageDiv1" : "messageDiv2"
@@ -44,14 +70,24 @@ const ConversationComponent = (props) => {
 
       <div className="chatBox">
         <div className="searchContainer">
+          {pickerVisible ? (
+            <Picker
+              onEmojiClick={onEmojiClick}
+              pickerStyle={{ position: "absolute", bottom: "60px" }}
+            />
+          ) : null}
           <img
             src="https://raw.githubusercontent.com/ayushkul/whatsapp-clone/facc8f2de4ca92aa9b6db5a931491d8d5be3be84/client/public/data.svg"
             alt="Chat Icon"
             className="emojiImage"
+            onClick={() => togglePicker(!pickerVisible)}
           />
           <input
             className="searchInput"
             placeholder="Type a message..."
+            value={text}
+            onKeyDown={onEnterPress}
+            onChange={(e) => setText(e.target.value)}
           ></input>
         </div>
       </div>
